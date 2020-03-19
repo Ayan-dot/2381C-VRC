2f7d7e5468da798cb25da94b362494bf9bd320bd
Edward <esong2@ocdsb.ca>
2020-03-18 22:45:40 -0400

    Implementation of polar coordinates

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -3,12 +3,13 @@
 #include "globals.hpp"
 
 void autonomous() {
-    
+    pros::Task intake_task(vector_tasks_fn, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT,"Print X and Y Task");
 }
 
-void robotVector() {
+void vector_tasks_fn(void *param) {
     double newAngle = 0, oldAngle = 0, angDiff = 0;
     double newEnc = 0, oldEnc = 0, enDiff = 0;
+    double angOrientation = 0;
 
     while(true) {
         oldAngle = inertial.get_heading();
@@ -22,9 +23,11 @@ void robotVector() {
         angDiff = newAngle - oldAngle;
         enDiff = newEnc - oldEnc;
 
-        positionTracking findPos(oldAngle, newAngle, enDiff, horizontalEncoder.get_value());
+        positionTracking findPos(oldAngle, newAngle, enDiff, horizontalEncoder.get_value(), angOrientation);
 
         pros::lcd::set_text(0, std::to_string((int) findPos.returnX()));
         pros::lcd::set_text(1, std::to_string((int) findPos.returnY()));
+
+        angOrientation = findPos.returnOrientation();
     }
 }
--- src/posTracking.cpp
+++ src/posTracking.cpp
@@ -4,18 +4,37 @@
 
 using namespace std;
 
+class motion {
+    private:
+        double desiredAngle = 0, distanceMove = 0;
+    public:
+        motion(double currentX, double currentY, double moveX, double moveY) {
+            desiredAngle = atan((moveY - currentY)/(moveX - currentX));
+            distanceMove = pow(pow((moveY - currentY), 2) + pow((moveX - currentX), 2), 0.5);
+        }
+
+        double returnAngle() {
+            return desiredAngle;
+        }
+
+        double returnDistance() {
+            return distanceMove;
+        }
+};
+
 class positionTracking {
     private:
         double angle = 0;
         double xtrans = 0, ytrans = 0;
         double xVec, yVec;
-        double angOrientation = 0;
+        double placeholder;
 
     public:
-        positionTracking(double oldAng, double newAng, double vertEncoder, double horiEncoder) {
-            angle = (newAng -  oldAng) * pi / 180 + angOrientation;
+        positionTracking(double oldAng, double newAng, double vertEncoder, double horiEncoder, double prevOrientation) {
+            angle = (newAng -  oldAng) * pi / 180 + prevOrientation;
             
-            angOrientation = angle;
+            prevOrientation = angle;
+            placeholder = prevOrientation;
 
             xtrans = vertEncoder / angle + verticalOffset;
             ytrans = horiEncoder / angle + horizontalOffset;
@@ -23,14 +42,19 @@ class positionTracking {
             xVec = 2 * sin(angle / 2 * xtrans);
             yVec = 2 * sin(angle / 2 * ytrans);
 
+
         }
 
         double returnX() {
             return xVec;
         }
-
+        
         double returnY() {
             return yVec;
+        }
+
+        double returnOrientation() {
+            return placeholder;
         }
 
 };
