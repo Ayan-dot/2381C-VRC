7c1ff65fd55c0eb9d2f7d09658e60c00310a3775
Edward <esong2@ocdsb.ca>
2020-03-22 15:56:59 -0400

    3/22 Ayan's new math

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -3,98 +3,96 @@
 #include "robot/intakes.cpp"
 #include "posTracking.cpp"
 
-void initialize() {
-   
+void initialize()
+{
+
     int time = pros::millis();
     int iter = 0;
     pros::lcd::initialize();
-    
+
+    master.clear();
+
     inertial.reset();
-    
-    while (inertial.is_calibrating()) {
+
+    while (inertial.is_calibrating())
+    {
         printf("IMU calibrating... %d\n", iter);
         iter += 10;
         pros::delay(10);
-        
     }
-    
+
     printf("IMU is done calibrating (took %d ms)\n", iter - time);
-    
-    
+
     // pros::Task intake_task(intake_tasks_fn, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT,"Intake Task");
     // pros::Task vector2_task(printVector_tasks_fn, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT,"Vector Task");
 }
 
-void intake_tasks_fn(void *param) {
-   while(true) {
+void intake_tasks_fn(void *param)
+{
+    while (true)
+    {
         Intakes run(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1), master.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
         rightIntake.move_voltage(run.rightSpeed());
         leftIntake.move_voltage(run.leftSpeed());
-   }
+    }
 }
 
 double currentx = 0, currenty = 0;
 
+void printVector_tasks_fn(void *param)
+{
 
+    while (true)
+    {
 
-void printVector_tasks_fn( void *param) {
-    
-    while (true) {
-        
-        
         leftFront.move(-1 * master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) + 0.8 * master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
         leftBack.move(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) + -0.8 * master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
         rightBack.move(-1 * master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) + -0.8 * master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
         rightFront.move(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) + 0.8 * master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
-        
-        
-        
     }
     pros::delay(10);
 }
 
-
-
-void opcontrol() {
+void opcontrol()
+{
     double newAngle = 0, oldAngle = 0, angDiff = 0;
     double newEnc = 0, oldEnc = 0, enDiff = 0, newEnc1 = 0, oldEnc1 = 0, enDiff1 = 0;
     double angOrientation = 0;
     double oldX = 0, oldY = 0; // these values must be changed to reflect our coordinate system
 
-    while(true) {
+    while (true)
+    {
         // oldAngle = inertial.get_heading();
-        oldEnc = (verticalEncoder.get_value()/360)*pi*3.25;
-        oldEnc1 = (horizontalEncoder.get_value()/360)*pi*2.75;
-        
+        oldEnc = (verticalEncoder.get_value() / 360) * pi * 3.25;
+        oldEnc1 = (horizontalEncoder.get_value() / 360) * pi * 2.75;
+
         pros::delay(10);
 
         newAngle = inertial.get_heading();
-        newEnc = verticalEncoder.get_value();
-        newEnc1 = verticalEncoder.get_value();
+        newEnc = (verticalEncoder.get_value() / 360) * pi * 3.25;
+        newEnc1 = (horizontalEncoder.get_value() / 360) * pi * 2.75;
 
         angDiff = newAngle - oldAngle;
         enDiff = newEnc - oldEnc;
-        enDiff1 = newEnc1 = oldEnc1;
+        enDiff1 = newEnc1 - oldEnc1;
 
+        positionTracking findPos(oldAngle, newAngle, angDiff, enDiff, enDiff1, oldX, oldY); // the last two variables are actually useless to pass to the function as of right now
+        // but since we might need to do something with them later they will stay there.
 
-        positionTracking findPos(oldAngle, newAngle, angDiff, enDiff, enDiff1, oldX, oldY);
-
-        
-        oldAngle = findPos.returnOrientation();
         oldX += findPos.returnX();
         oldY += findPos.returnY();
+        oldAngle = findPos.returnOrientation() * 180 / pi;
 
-        pros::delay(10);
-        std::string d1 = std::to_string(verticalEncoder.get_value());
-		pros::lcd::set_text(0, d1);
-		std::string d2 = std::to_string(horizontalEncoder.get_value());
-		pros::lcd::set_text(1, d2);
-		std::string d3 = std::to_string(inertial.get_heading());
-		pros::lcd::set_text(2, d3);
-		
-        
-        pros::delay(10);
-        
+        master.print(0, 0, "V: %f", oldX);
+        pros::delay(50);
+
+        master.print(1, 0, "H: %f", oldY);
+        pros::delay(50);
+
+        master.print(0, 0, "I: %f", oldAngle);
+        pros::delay(50);
+
+
         
     }
 }
--- src/posTracking.cpp
+++ src/posTracking.cpp
@@ -4,56 +4,84 @@
 
 using namespace std;
 
-class motion {
-    private:
-        double desiredAngle = 0, distanceMove = 0;
-    public:
-        motion(double currentX, double currentY, double moveX, double moveY) {
-            desiredAngle = atan((moveY - currentY)/(moveX - currentX));
-            distanceMove = pow(pow((moveY - currentY), 2) + pow((moveX - currentX), 2), 0.5);
-        }
+class motion
+{
+private:
+    double desiredAngle = 0, distanceMove = 0;
 
-        double returnAngle() {
-            return desiredAngle;
-        }
+public:
+    motion(double currentX, double currentY, double moveX, double moveY)
+    {
+        desiredAngle = atan((moveY - currentY) / (moveX - currentX));
+        distanceMove = pow(pow((moveY - currentY), 2) + pow((moveX - currentX), 2), 0.5);
+    }
 
-        double returnDistance() {
-            return distanceMove;
-        }
+    double returnAngle()
+    {
+        return desiredAngle;
+    }
+
+    double returnDistance()
+    {
+        return distanceMove;
+    }
 };
 
-class positionTracking {
-    private:
-        double angle = 0;
-        double displacement = 0;
-        double xVec = 0, yVec = 0;
+class positionTracking
+{
+private:
+    double angle = 0;
+    double displacement = 0;
+    double xVec = 0, yVec = 0;
+    double sum = 0;
 
-    public:
-        positionTracking(double oldAng, double newAng, double angDiff, double vertEncoder, double horiEncoder, double oldX, double oldY) {
-        angle = newAng*pi/180.0;
-        oldAng = oldAng*pi/180.0;
-        angDiff = angDiff*pi/180.0;
-        displacement = 2*sin(angDiff/2)*(vertEncoder/angDiff);
-        // field centric;
-        xVec = displacement * cos(oldAng+angDiff/2.0);
-        yVec = displacement * sin(oldAng+angDiff/2.0);
+public:
+    positionTracking(double oldAng, double newAng, double angDiff, double vertEncoder, double horiEncoder, double oldX, double oldY)
+    {
+        angle = newAng * pi / 180.0;
 
-        displacement = 2*sin(angDiff/2)*(horiEncoder/angDiff+horizontalOffset);
-        xVec += displacement * cos(oldAng+angDiff/2.0);
-        yVec += displacement * -sin(oldAng+angDiff/2.0);
+        oldAng = oldAng * pi / 180.0;
+        angDiff = angDiff * pi / 180.0;
 
+        sum = oldAng + angDiff;
+        // parallel wheel portion
+        if (angDiff != 0)
+        {
+            displacement = 2 * sin(angDiff / 2) * (vertEncoder / angDiff);
+            // field centric;
+            xVec = displacement * cos(sum / 2.0);
+            yVec = displacement * sin(sum / 2.0);
+            // back wheel portion
+            displacement = 2 * sin(angDiff / 2) * (horiEncoder / angDiff + horizontalOffset);
+            //  field centric
+            xVec += displacement * cos(sum / 2.0);
+            yVec += displacement * -sin(sum / 2.0);
         }
-
-        double returnX() {
-            return xVec;
+        else
+        {
+            displacement = vertEncoder;
+            xVec = displacement * cos(oldAng);
+            yVec = displacement * sin(oldAng);
+            
+            displacement = horiEncoder;
+            
+            xVec += displacement * cos(oldAng);
+            yVec += displacement * -sin(oldAng);
         }
+    }
 
-        double returnY() {
-            return yVec;
-        }
+    double returnX()
+    {
+        return xVec;
+    }
 
-        double returnOrientation() {
-            return angle;
-        }
+    double returnY()
+    {
+        return yVec;
+    }
 
+    double returnOrientation()
+    {
+        return angle;
+    }
 };
