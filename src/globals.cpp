28e8ba7a7dc8e47cc0e25fc06e5877b2d8221638
Ayan-dot <62365810+Ayan-dot@users.noreply.github.com>
2020-06-26 20:33:57 -0400

    added comment

--- src/globals.cpp
+++ src/globals.cpp
@@ -21,7 +21,7 @@ std::array<double, 3> drivebasePIDParams = {0.032, 0, 0};
 
 int maxSpeed = 12000;
 double verticalOffset = 0; // needs to be changed depending on vertical tracking wheel placement
-double horizontalOffset = 0.5;
+double horizontalOffset = 0.5; // see above
 double horiToInch = (pi*2.75)/360.0;
 double vertToInch = (pi*3.25)/360.0;
 double imuToRad = pi/180.0;
