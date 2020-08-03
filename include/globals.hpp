06f83366a6c1a548eccf6b3b0cf144166870ccb9
allenapplehead <allentao7@gmail.com>
2020-08-03 17:05:28 -0400

    Working automatic ball indexer, along with driver controls, and pretty accurate odometry

--- include/globals.hpp
+++ include/globals.hpp
@@ -5,26 +5,32 @@
 
 extern pros::Controller master;
 extern pros::Motor leftBack, leftFront, rightBack, rightFront;
-extern pros::Motor leftIntake, rightIntake;
+extern pros::Motor leftIntake, rightIntake, indexer, shooter;
 extern pros::Motor lift;
 extern pros::Imu inertial;
 
 extern std::array<double, 3> anglerPIDParams;
 extern std::array<double, 3> drivebasePIDParams;
+extern std::array<double, 3> turningPID;
+extern std::array<double, 3> adjustmentPIDParams;
 
 extern int maxSpeed;
+extern int INDEX_THRESHOLD;
 
-extern double verticalOffset;
+extern double verticalOffset1;
+extern double verticalOffset2;
 extern double horizontalOffset;
 extern double horiToInch;
 extern double vertToInch;
 extern double imuToRad;
+extern double globalX, globalY;
 
 extern const double pi;
 
-extern pros::ADIEncoder verticalEncoder;
+extern pros::ADIEncoder verticalEncoder1;
+extern pros::ADIEncoder verticalEncoder2;
 extern pros::ADIEncoder horizontalEncoder;
+extern pros::ADIAnalogIn line_tracker;
 
 
 #endif
-
--- src/globals.cpp
+++ src/globals.cpp
@@ -3,31 +3,64 @@
 #include "globals.hpp"
 #include "pid.hpp"
 
-
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::Motor leftBack(12);
-pros::Motor leftFront(11);
-pros::Motor rightBack(19);
-pros::Motor rightFront(20);
-pros::Motor leftIntake(10);
-pros::Motor rightIntake(15);
-pros::Motor lift(14);
-pros::Imu inertial(21);
-pros::ADIEncoder verticalEncoder('A','B', false);
-pros::ADIEncoder horizontalEncoder('C','D', false);
-
+pros::Motor leftBack(2);
+pros::Motor leftFront(12);
+pros::Motor rightBack(9);
+pros::Motor rightFront(19);
+pros::Motor leftIntake(15);
+pros::Motor rightIntake(16);
+pros::Motor indexer(3);
+pros::Motor shooter(17);
+pros::Imu inertial(1);
+pros::ADIEncoder verticalEncoder1('E','F', false);
+pros::ADIEncoder verticalEncoder2('A','B', false);
+pros::ADIEncoder horizontalEncoder('C','D', true);
+pros::ADIAnalogIn line_tracker('G');
 std::array<double, 3> anglerPIDParams = {0.07, 0, 0};
-std::array<double, 3> drivebasePIDParams = {0.032, 0, 0};
+std::array<double, 3> drivebasePIDParams = {23, 0, 0};
+std::array<double, 3> turningPID = {160, 0, 170};
+std::array<double, 3> adjustmentPIDParams = {2,0,0};
 
 int maxSpeed = 12000;
-double verticalOffset = 0; // needs to be changed depending on vertical tracking wheel placement
-double horizontalOffset = 0.5; // see above
+int INDEX_THRESHOLD = 2600; // needs to be changed depending on ambient lighting conditions
+double verticalOffset1 = 6.1675; // needs to be changed depending on vertical tracking wheel placement
+double verticalOffset2 = 6.1675;
+double horizontalOffset = 6.8075;
 double horiToInch = (pi*2.75)/360.0;
-double vertToInch = (pi*3.25)/360.0;
+double vertToInch = (pi*2.75)/360.0;
 double imuToRad = pi/180.0;
+double globalX = 0, globalY = 0;
 
 const double pi = 3.14159265358979323846;
 
 void disabled() {}
 
 void competition_initialize() {}
+
+
+void initialize()
+{
+
+    int time = pros::millis();
+    int iter = 0;
+
+    pros::lcd::initialize();
+
+    inertial.reset();
+    while (inertial.is_calibrating()) {
+      pros::lcd::set_text(7, "IMU calibrating ...");
+      iter += 10;
+      pros::delay(10);
+    }
+    // should print about 2000 ms
+    pros::lcd::set_text(7, "IMU is done calibrating (took %d ms)\n" + std::to_string(iter - time));
+
+    verticalEncoder1.reset();
+    verticalEncoder2.reset();
+    horizontalEncoder.reset();
+
+    master.clear();
+
+
+}
